0583563cf0bd0e62305cae5c5cb40d203b735eca
𝐇𝐚𝐫𝐬𝐡 𝐌𝐚𝐤𝐡𝐥𝐨𝐠𝐚 <91483777+HARSH99MA@users.noreply.github.com>
2026-07-15 17:04:21 +0530

    Time: 0 ms (100%), Space: 44.7 MB (48.34%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,18 +1,11 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int found = 0;
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target) {
-                found = i;
-            }
-            else if(nums[i] < target){
-                found = i+1;
-            }
-            else if(target > nums[i]){
-                found = i + 1;
+            if (nums[i] >= target) {
+                return i;
             }
            
         }
-        return found;
+        return nums.length;
     }
 }
